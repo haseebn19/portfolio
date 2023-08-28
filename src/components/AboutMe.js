a33bdcf0027628f7c143f20a1c420845d29fe241
Muhammad Haseeb Niazi <haseebniazi19@outlook.com>
2023-08-28 04:50:15 -0400

    Updated files

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,7 +1,7 @@
 // Importing React
 import React from 'react';
 // Importing the image
-import profilePic from '../assets/images/logo.jpg';
+import profilePic from '../assets/images/picture.jpg';
 // Importing font awesome
 import 'font-awesome/css/font-awesome.min.css';
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,32 +1,49 @@
+// Import React and useState hook from React library
 import React, {useState} from 'react';
 
-// Dynamically import all files from the 'projects' folder
+// Dynamically import all JavaScript files from the 'projects' folder
+// This will return an array of filenames that can be used to import the components
 const projectComponents = require.context('./projects', true, /\.js$/);
 const projectFiles = projectComponents.keys();
 
+// Define the Projects component
 const Projects = () => {
+  // Declare a state variable to keep track of the currently expanded project
   const [expandedProject, setExpandedProject] = useState(null);
 
+  // Function to toggle the expanded project
   const toggleProject = (index) => {
+    // If the clicked project is already expanded, collapse it
     if (expandedProject === index) {
       setExpandedProject(null);
     } else {
+      // Otherwise, expand the clicked project
       setExpandedProject(index);
     }
   };
 
+  // Render the Projects component
   return (
     <div>
       <h2>My Projects</h2>
       <ul>
+        {/* Loop through the list of project filenames */}
         {projectFiles.map((filename, index) => {
+          // Dynamically import the project component based on the filename
           const ProjectComponent = projectComponents(filename).default;
+
+          // Get the display name for the project, if specified, otherwise use the filename
           const displayName = projectComponents(filename).projectName || filename.replace('./', '').replace('.js', '');
+
+          // Render each project as a list item
           return (
             <li key={index}>
+              {/* Button to toggle the expanded/collapsed state of the project */}
               <button onClick={() => toggleProject(index)}>
                 {displayName} {expandedProject === index ? '▲' : '▼'}
               </button>
+
+              {/* Render the project component if it is expanded */}
               {expandedProject === index && <ProjectComponent />}
             </li>
           );
@@ -36,4 +53,5 @@ const Projects = () => {
   );
 };
 
+// Export the Projects component to be used in other parts of the app
 export default Projects;
