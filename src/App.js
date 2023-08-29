87744221be912de623a482d34bcf1a7d0e3c8462
Muhammad Haseeb Niazi <haseebniazi19@outlook.com>
2023-08-28 20:15:52 -0400

    Update files

--- src/App.js
+++ src/App.js
@@ -1,44 +1,29 @@
-// Import React library and various components from react-router-dom
 import React from 'react';
-import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
-
-// Import custom components for AboutMe and Projects
+import {BrowserRouter as Router, Route, Link, Routes, Navigate} from 'react-router-dom';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
 
-// Define the App component
 function App() {
   return (
-    // Use Router to enable client-side routing
     <Router>
       <div className="container">
-        {/* Display the main title */}
         <h1>Haseeb Niazi</h1>
-
-        {/* Navigation menu */}
         <nav>
           <ul>
-            {/* Link to the Home page */}
             <li>
-              <Link className="nav-link" to={process.env.PUBLIC_URL + "/"}>Home</Link>
+              <Link className="nav-link" to="/">Home</Link>
             </li>
-
-            {/* Link to the Projects page */}
             <li>
-              <Link className="nav-link" to={process.env.PUBLIC_URL + "/projects"}>Projects</Link>
+              <Link className="nav-link" to="/projects">Projects</Link>
             </li>
           </ul>
         </nav>
-
-        {/* Main content frame */}
         <div className="frame">
-          {/* Define routes for different pages */}
           <Routes>
-            {/* Route for the Home page, renders the AboutMe component */}
-            <Route path={process.env.PUBLIC_URL + "/"} element={<AboutMe />} />
-
-            {/* Route for the Projects page, renders the Projects component */}
-            <Route path={process.env.PUBLIC_URL + "/projects"} element={<Projects />} />
+            <Route index element={<Navigate to="/aboutme" />} />
+            <Route path="/aboutme" element={<AboutMe />} />
+            <Route path="/projects" element={<Projects />} />
+            {/* Add a 404 page here if needed */}
           </Routes>
         </div>
       </div>
@@ -46,5 +31,4 @@ function App() {
   );
 }
 
-// Export the App component to be used in other parts of the app
 export default App;
